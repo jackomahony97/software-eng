1926472aa9ff6e3f18494f9c357b0cbb4b3fee93
jackomahony97 <37781244+jackomahony97@users.noreply.github.com>
2019-11-21 14:18:44 +0000

    Update tokenizer.h

--- tokenizer.h
+++ tokenizer.h
@@ -1,10 +1,13 @@
-//
-// Created by jacko on 20/11/2019.
-//
+/**
+* Header file for tokenizer.c
+* @author Jack O Mahony (117498152)
+* @date 13/11/2019
+*/
 
 #ifndef CALC_TOKENIZER_H
 #define CALC_TOKENIZER_H
 
+/** Method definition */
 void tokenizer(void);
 
 #endif //CALC_TOKENIZER_H
