2b1f65c86c596469220f49b965e91d14d5371a6e
jackomahony97 <37781244+jackomahony97@users.noreply.github.com>
2019-11-21 14:17:40 +0000

    Update infix2postfix.h

--- infix2postfix.h
+++ infix2postfix.h
@@ -1,10 +1,13 @@
-//
-// Created by jacko on 20/11/2019.
-//
+/**
+* Header file for infix2postfix.c
+* @author Jack O Mahony (117498152)
+* @date 13/11/2019
+*/
 
 #ifndef CALC_INFIX2POSTFIX_H
 #define CALC_INFIX2POSTFIX_H
 
+/** Method definition */
 int infix2postfix(void);
 
 #endif //CALC_INFIX2POSTFIX_H
