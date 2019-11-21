411fdc4d2c40f56745a10fb01a874f8eaa8300b1
jackomahony97 <37781244+jackomahony97@users.noreply.github.com>
2019-11-21 14:19:57 +0000

    Update virtualmachine.h

--- virtualmachine.h
+++ virtualmachine.h
@@ -1,10 +1,13 @@
-//
-// Created by jacko on 20/11/2019.
-//
+/**
+* Header file for virtualmachine.c
+* @author Jack O Mahony (117498152)
+* @date 13/11/2019
+*/
 
 #ifndef CALC_VIRTUALMACHINE_H
 #define CALC_VIRTUALMACHINE_H
 
+/** Method definition */
 int virtualmachine(void);
 
 #endif //CALC_VIRTUALMACHINE_H
