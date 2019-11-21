4a1106727b6bf4267dd45c2f202168e0c480ff7b
jackomahony97 <37781244+jackomahony97@users.noreply.github.com>
2019-11-21 14:16:35 +0000

    Update codegenerator.h

--- codegenerator.h
+++ codegenerator.h
@@ -1,10 +1,13 @@
-//
-// Created by jacko on 20/11/2019.
-//
+/**
+* Header file for codegenerator.c
+* @author Jack O Mahony (117498152)
+* @date 13/11/2019
+*/
 
 #ifndef CALC_CODEGENERATOR_H
 #define CALC_CODEGENERATOR_H
 
+/** Method defintion*/
 void codegenerator(void);
 
 #endif //CALC_CODEGENERATOR_H
