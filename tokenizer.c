69b4bceed6939b60c2a72d45d6088bf3c1b41beb
jackomahony97 <37781244+jackomahony97@users.noreply.github.com>
2019-11-25 19:29:47 +0000

    Update tokenizer.c

--- tokenizer.c
+++ tokenizer.c
@@ -1,101 +1,39 @@
+//
+// Created by jacko on 20/11/2019.
+//
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
+#include <ctype.h>
+#include "tokenizer.h"
 
+void tokenizer() {
+    FILE *infile, *outfile;
+    infile = fopen("./temp/input.txt", "r");
+    outfile = fopen("./temp/tokenizer.txt", "w+");
+    char line[1024];
 
-bool checkBrackets(char line[]){
-    int openBrackets = 0;
-    int closeBrackets = 0;
-    for(int i = 0; line[i]; i++){
-        if (line[i] == '('){
-            openBrackets += 1;
-        }
-        if (line[i] == ')'){
-            closeBrackets += 1;
-        }
-    }
-    if (openBrackets == closeBrackets){
-       return true;
-    }
-    else{
-        return false;
-    }
-}
-
-bool checkOperators(char line[]){
-    for(int i = 0; line[i]; i++){
+    fgets(line, 1024, infile); // reads in first line fom input file
+    for (int i = 0; line[i]; i++) {
         if (line[i] == '+'
             || line[i] == '-'
             || line[i] == '/'
             || line[i] == '*'
+            || line[i] == '%'
             || line[i] == '^'
-            || line[i] == '%'){
-            if (!(line[i+1] == '('
-                || line[i+1] == ')'
-                || isdigit(line[i+1]))
-                ){
-                return false;
-            }
+            || line[i] == '('
+            || line[i] == ')'
+            || line[i] == '\n'
+            || line[i] == '\r'
+                ) {
+            char c = '\n';
+            fprintf(outfile,"%c ",c);
+            fprintf(outfile,"%c ",line[i]);
+            fprintf(outfile,"%c ",c);
+        } else if ((isdigit(line[i])) || (line[i] == '.')) {
+            fprintf(outfile,"%c ",line[i]);
         }
     }
-    return true;
-
-}
-
-bool checkSyntax(char line[]){
-    if (checkOperators(line)
-        &&checkBrackets(line)
-        )
-        {
-        return true;
-
-    }
-    else{
-        return false;
-    }
-
-}
-
-
-
-
-main()
-{
-    FILE *infile, *outfile;
-    infile = fopen("input.txt","r");
-    outfile = fopen("postfix.txt", "w+");
-    char line[1024];
-    fgets( line ,1024, infile); // reads in first line from input file
-    if (checkSyntax(line)){ // counts the number of ( and ) symbols and returns syntax error if they are unequal.
-
-        for(int i = 0; line[i]; i++){
-            char newline = '\n';
-            if (line[i] == '+'
-                || line[i] == '-'
-                || line[i] == '/'
-                || line[i] == '*'
-                || line[i] == '('
-                || line[i] == ')'
-                || line[i] == '%'
-                || line[i] == '^'
-                || line[i] == '\n'
-                || line[i] == '\r'
-                )
-                {
-                    fputc(line[i], outfile);
-                    fputc(newline, outfile);
-            }
-            else if ( (isdigit(line[i])) || (line[i] == '.') ) {
-				fputc(line[i], outfile);
-                if (!(line[i+1] == '.'|| isdigit(line[i+1]))){
-                    fputc(newline, outfile);
-                }
-            }
-        }
-    }
-
-    else {
-        fputs("syntax error", outfile);
-    }
-    exit;
+    fclose(infile);
+    fclose(outfile);
 }
