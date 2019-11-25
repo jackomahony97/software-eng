c41564ab003e1b7fa05f66d7acf2a505413fd192
jackomahony97 <37781244+jackomahony97@users.noreply.github.com>
2019-11-25 19:25:59 +0000

    Update main.c

--- main.c
+++ main.c
@@ -22,23 +22,19 @@ int main( )
     char line[1024]; /** line buffer */
     FILE *infile, *outfile; 
 
-    mkdir("../temp"); /** make a directory */
+    mkdir("./temp", S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH); /** make a directory */
     printf( "Enter a path :"); 
     scanf("%s", path); /** get file path from user (testing ../example.txt) */
     infile = fopen(path,"r"); /** open input file (read only) */
 
     while(fgets(line,sizeof (line),infile)!= NULL)  { /** for each line in input file */
-        outfile = fopen("../temp/input.txt", "w"); /** open output file (write only) */
+        outfile = fopen("./temp/input.txt", "w+"); /** open output file (write only) */
         fprintf(outfile, "%s\n", line); /** write line to output file  */
         fclose(outfile); /** close output file */
         tokenizer(); /** call tokenizer() */
         infix2postfix(); /** call infix2postfix() */
         codegenerator(); /** call codegenerator */
         virtualmachine(); /** call virtualmachine */
-        remove("../temp/input.txt"); /** remove file */
-        remove("../temp/tokenizer.txt"); /** remove file */
-        remove("../temp/postfix.txt"); /** remove file */
-        remove("../temp/instructions.txt"); /** remove file */
     }
     fclose(infile); /** close input file */
     return 0;
