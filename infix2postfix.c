6762d3f1b606a137f688a4436422d708fbd5ba6b
hoanganh04 <32456445+hoanganh04@users.noreply.github.com>
2019-11-25 22:34:22 +0000

    Update infix2postfix.c

--- infix2postfix.c
+++ infix2postfix.c
@@ -1,7 +1,11 @@
 //
 // Created by jacko on 20/11/2019.
 //
-
+/**
+* The infix2postfix program reads an input file of tokenizer and convert them to postfix format using a stack and writes the result to an postfix file.
+* @author Hoang Anh Mai (117101156)
+* @date 25/11/2019
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
@@ -9,27 +13,30 @@
 
 #define STKSZ 100
 
+//Init a struct for stack
 struct stack { char items[STKSZ]; int top;} stk;
 
+//Function to push element to the stack if the stack is full and push() is called print out overflow error.
 void push(char ele)
 {
     if (stk.top + 1 == STKSZ) {
-        printf("\n[E] Stack Overflow!\n");
+        printf("\n Stack Overflow!\n");
         exit(1);
     }
     stk.items[++stk.top] = ele;
 }
 
+//Function to pop element to the stack if there is no element left and pop() is called print out underflow error.
 char pop()
 {
     if (stk.top == -1) {
-        printf("\n[E] Stack Underflow!\n");
+        printf("\n Stack Underflow!\n");
         exit(1);
     }
     return (stk.items[stk.top--]);
 }
 
-
+//Set priority for operators in the stack.
 int priority(char c)
 {
     switch (c)
@@ -41,6 +48,7 @@ int priority(char c)
     }
 }
 
+//Main function.
 int infix2postfix(void)
 {
     stk.top=-1;
@@ -48,6 +56,7 @@ int infix2postfix(void)
     char expr[60],*e, x,c;
     infile = fopen("./temp/tokenizer.txt","r");
     outfile= fopen("./temp/postfix.txt","w+");
+// If infile is available loop through each character in the file and convert the file to postfix. Return 1 if no error.    
     if (infile) {
         while ((c = getc(infile)) != EOF) {
             if ( (isalnum(c)) || (c == '.') ) {
@@ -69,8 +78,10 @@ int infix2postfix(void)
             }
         }
         while (stk.top>-1) {fprintf(outfile,"\n%c", pop());}
+        return 1;
     }
     fclose(infile);
     fclose(outfile);
+//Else return 0.
     return 0;
 }
