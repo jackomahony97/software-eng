a9a7ae34ca97f51026d5bd7a6fc47854897e7009
hoanganh04 <32456445+hoanganh04@users.noreply.github.com>
2019-11-26 17:36:42 +0000

    Update main.c

--- jjjj/main.c
+++ jjjj/main.c
@@ -20,6 +20,7 @@ int main( )
     */
     char path[100]; /** file path */
     char line[1024]; /** line buffer */
+    int result;
     FILE *infile, *outfile; 
 
     mkdir("./temp", S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH); /** make a directory */
@@ -28,14 +29,22 @@ int main( )
     infile = fopen(path,"r"); /** open input file (read only) */
 
     while(fgets(line,sizeof (line),infile)!= NULL)  { /** for each line in input file */
+	
         outfile = fopen("./temp/input.txt", "w+"); /** open output file (write only) */
         fprintf(outfile, "%s\n", line); /** write line to output file  */
         fclose(outfile); /** close output file */
-        tokenizer(); /** call tokenizer() */
-        infix2postfix(); /** call infix2postfix() */
-        codegenerator(); /** call codegenerator */
-        virtualmachine(); /** call virtualmachine */
+        result = tokenizer(); /** call tokenizer() */
+	if (result == 0) {
+        	result = infix2postfix(); /** call infix2postfix() */
+		if (result == 0) {
+        		result = codegenerator(); /** call codegenerator */
+			if (result == 0) {
+        			result = virtualmachine(); /** call virtualmachine */
+			}
+		}
+	}
     }
+    printf("%d",result);
     fclose(infile); /** close input file */
     return 0;
 }
