b0eddaa7343fead68deb0553a70d273f21b54d60
kevinc-303 <57999217+kevinc-303@users.noreply.github.com>
2019-11-26 15:33:08 +0000

    Commented

--- codegenerator.c
+++ codegenerator.c
@@ -1,67 +1,70 @@
-
-//
-// Created by jacko on 20/11/2019.
-//
+/*
+Code Generator
+Author: Kevin O' Donovan
+The Code Generator program takes the output file from the infix2postfix program as input.
+It will then convert each token into an instruction, which will be outputted in an "instructions" file.
+The HLT(Halt) instruction will be appended to the end of the output file.
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "codegenerator.h"
 
-void codegenerator() {
+void codegenerator() {  /* This file comprises of 1 function which translates the input file into a single output file */
 
-    FILE *infile, *outfile;
-    char c[100]; /* or other suitable maximum line size */
-    int result;
-    char loadnum[100] = "LOADNUM, ";
+    FILE *infile, *outfile;  /* Initialise input and output files */
+    char c[100];  /* Declare maximum token size */
+    int result;  /* Initialise integer to hold result of string comparisons */
+    char loadnum[100] = "LOADNUM, ";  /* Initialise string for LOADNUM instructions */
 
-    infile = fopen("./temp/postfix.txt", "r");
-    outfile = fopen("./temp/instructions.txt", "w+");
+    infile = fopen("./temp/postfix.txt", "r");  /* Open input file, create if non-existent */
+    outfile = fopen("./temp/instructions.txt", "w+");  /* Open output file, create if non-existant */
 
     if (infile == NULL) {
         printf("ERROR: Unable To Access Postfix File\n");
-        exit(1);
+        exit(1);  /* If unable to access the output of infix2postfix, the program will exit and an error will display. */
     }
 
     while (fgets(c, sizeof c, infile) != NULL) {
-        strtok(c, "\n");
+        strtok(c, "\n");  /* Strip the newline character from each token - newline character will not be removed if it is the only character in that line. */
 
         result = strcmp(c, "+");
         if (result == 0) {
-            fputs("ADD,\n", outfile);
+            fputs("ADD,\n", outfile);  /* Compare with '+' operator. If true, print 'ADD' instruction. */
         } else {
             result = strcmp(c, "-");
             if (result == 0) {
-                fputs("SUB,\n", outfile);
+                fputs("SUB,\n", outfile);  /* Compare with '-' operator. If true, print 'SUB' instruction. */
             } else {
                 result = strcmp(c, "*");
                 if (result == 0) {
-                    fputs("MUL,\n", outfile);
+                    fputs("MUL,\n", outfile);  /* Compare with '*' operator. If true, print 'MUL' instruction. */
                 } else {
                     result = strcmp(c, "/");
                     if (result == 0) {
-                        fputs("DIV,\n", outfile);
+                        fputs("DIV,\n", outfile);  /* Compare with '/' operator. If true, print 'DIV' instruction. */
                     } else {
                         result = strcmp(c, "^");
                         if (result == 0) {
-                            fputs("EXP,\n", outfile);
+                            fputs("EXP,\n", outfile);  /* Compare with '^' operator. If true, print 'EXP' instruction. */
                         } else {
                             result = strcmp(c, "%");
                             if (result == 0) {
-                                fputs("MOD,\n", outfile);
+                                fputs("MOD,\n", outfile);  /* Compare with '%' operator. If true, print 'MOD' instruction. */
                             } else {
                                 result = strcmp(c, "\n");
                                 if (result == 0) {
-                                    fputs("", outfile);
+                                    fputs("", outfile);  /* Compare with newline character. If true, print empty string. */
                                 } else {
                                     result = strcmp(c, "");
                                     if (result == 0) {
-                                        fputs("", outfile);
+                                        fputs("", outfile);  /* Compare with empty string. If true, print empty string. */
                                     } else {
-                                        strcat(loadnum, c);
-                                        strcat(loadnum, ",\n");
-                                        fputs(loadnum, outfile);
-                                        strcpy(loadnum, "LOADNUM, ");
+                                        strcat(loadnum, c);  /* Concatenate token to loadnum string. */
+                                        strcat(loadnum, ",\n");  /* Concatenate newline character to loadnum string. */
+                                        fputs(loadnum, outfile);  /* Print LOADNUM instruction to outfile. */
+                                        strcpy(loadnum, "LOADNUM ");  /* Reset loadnum string to initial value . */
                                     }
                                 }
                             }
@@ -73,8 +76,8 @@ void codegenerator() {
 
     }
 
-    fputs("HLT,", outfile);
+    fputs("HLT,", outfile);  /* Append Halt instruction to the end of the program */
 
-    fclose(infile);
-    fclose(outfile);
+    fclose(infile);  /* Close input file */
+    fclose(outfile);  /* Close output file */
 }
