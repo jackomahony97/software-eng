27545f6320b2cea3b594d240267884e21f830c1e
jackomahony97 <37781244+jackomahony97@users.noreply.github.com>
2019-11-26 17:39:49 +0000

    Add files via upload

---
+++ test.c
@@ -0,0 +1,23 @@
+/**
+* Test uses TAP (Test Anyhing Protocal) to test calc
+* @author Jack O Mahony (117498152)
+* @date 13/11/2019
+*/
+/*
+#include <ctap.h>
+#include "virtualmachine.h"
+#include "tokenizer.h"
+#include "codegenerator.h"
+#include "infix2postfix.h"
+#include "virtualmachine.h"
+#include "tokenizer.h"
+#include "codegenerator.h"
+#include "infix2postfix.h"
+
+
+TESTS {
+
+    ok(1 == 1, "1 does in fact equal 1");
+}
+ **/
+
